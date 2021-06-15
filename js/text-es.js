50e58ce2d77102e4fd412ef2378db0866659834e
Ariel Martin <arielmartin80@gmail.com>
2021-06-14 22:28:37 -0300

    correccion imagen

--- js/text-es.js
+++ js/text-es.js
@@ -6,7 +6,7 @@ let subtitle1 = "Este es el subtitulo de la página";
 
 let logo = "images/logo.svg";
 //let url_background = "https://images.pexels.com/photos/34600/pexels-photo.jpg";
-let url_background = "../images/header-background.jpg";
+let url_background = "images/header-background.jpg";
  
 
 export{title1, subtitle1, logo, url_background};
